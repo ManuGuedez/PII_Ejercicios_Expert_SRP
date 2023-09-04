f609360bd1fa85e272c214d7529d7107c002cc1c
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2023-09-04 18:52:51 -0300

    .

--- Ejercicio 1/src/Library/Sector.cs
+++ Ejercicio 1/src/Library/Sector.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+
 namespace Library
 {
     public class Sector
     {
         public string SectorName { get ; set ;}
+        public List<Shelves> Shelves { get ; set ; }
+        public Sector(string name, List<Shelves> shelves)
+        {
+            SectorName = name;
+            Shelves = shelves;
+        }
     }
 }
--- Ejercicio 1/src/Library/Shelves.cs
+++ Ejercicio 1/src/Library/Shelves.cs
@@ -1,13 +1,34 @@
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using SRP;
 
 namespace Library
 {
     public class Shelves
     {
+        private static double shelfNumber = 0;
         public List<Book> Books { get ; set; }
 
-        public int ShelfNumber { get ; set; } 
+        public double ShelfNumber { get ; private set ; } 
+        public Shelves(List<Book> books)
+        {
+            this.Books = books;
+            ShelfNumber = shelfNumber;
+            shelfNumber++;
+        }
 
+        public Shelves()
+        {
+            return ; 
+        }
+        public double Lenght()
+        {
+            return Books.Count;
+        }
+
+        public Book BookIndex(int indice)
+        {
+            return Books[indice];
+        }
     }
 }
