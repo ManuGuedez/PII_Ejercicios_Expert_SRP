43ba80ec1cd203f1cdb6972109940d0a728821fc
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2023-09-04 20:57:03 -0300

    .

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,10 +7,14 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
+            Patient person1 = new("paciente1", "5555-555-555", "54673456", 21);;
+            Doctor doctor = new("Pepe", "neurólogo");
+
+            string appointmentResult = AppointmentService3.CreateAppointment(person1, doctor, DateTime.Now, "Wall Street");
             Console.WriteLine(appointmentResult);
 
-            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
+            Patient person2 = new("paciente2", "6666-666-666", "32342342", 32);
+            string appointmentResult2 = AppointmentService3.CreateAppointment(person1, doctor, DateTime.Now, "Queen Street");
             Console.WriteLine(appointmentResult2);
         }
     }
